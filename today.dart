5ae9dd7a1253923dce1034d0cb474d7dbf3288ae
Ahmed Raza <100880841+ahmedrazamadani@users.noreply.github.com>
2022-03-20 13:02:09 +0500

    Add files via upload

---
+++ today.dart
@@ -0,0 +1,32 @@
+
+
+
+void main (){
+
+// for (int i = 1 ; i <= 10 ; i++){
+
+// print("7 * $i = ${7*i}");
+
+// } 
+// for (var i = 0 ; i<10; i++){
+
+//   print (i);
+// }   
+// List abc = ["Qasim","Owais" , "Qadeer" , "Majid"];
+// for (var i = 0 ; i<abc.length; i++){
+
+//   print("$i ${abc[i]}");
+// }
+//  
+// }
+
+// var num = [35,345,23,26,12,8,6,5];
+// for (var i = 0 ; i< num.length; i++){
+// if (num [i] % 2 ==0  )
+// {print(num [i]);}
+// else {print(false);}
+// }
+
+
+
+}
